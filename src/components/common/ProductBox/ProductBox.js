df10e01fed8de74b63aeaa1440a89cdb92414141
Mikolaj-Krawiec <59422679+Mikolaj-Krawiec@users.noreply.github.com>
2020-07-12 23:44:59 +0200

    Add Stars component

--- src/components/common/ProductBox/ProductBox.js
+++ src/components/common/ProductBox/ProductBox.js
@@ -1,15 +1,12 @@
-import React, { useState } from 'react';
+import React from 'react';
 import PropTypes from 'prop-types';
 
 import styles from './ProductBox.module.scss';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import {
-  faStar,
-  faExchangeAlt,
-  faShoppingBasket,
-} from '@fortawesome/free-solid-svg-icons';
-import { faStar as farStar, faHeart } from '@fortawesome/free-regular-svg-icons';
+import { faExchangeAlt, faShoppingBasket } from '@fortawesome/free-solid-svg-icons';
+import { faHeart } from '@fortawesome/free-regular-svg-icons';
 import Button from '../Button/Button';
+import Stars from '../Stars/StarsContainer';
 
 const ProductBox = ({
   id,
@@ -24,7 +21,6 @@ const ProductBox = ({
   userStars,
   ...props
 }) => {
-  const [hover, setHover] = useState(null);
   return (
     <div className={styles.root}>
       <div className={styles.photo}>
@@ -41,54 +37,7 @@ const ProductBox = ({
       </div>
       <div className={styles.content}>
         <h5>{name}</h5>
-        <div className={styles.stars}>
-          {[1, 2, 3, 4, 5].map(i => {
-            let count = stars;
-            if (userStars) {
-              count = userStars;
-            }
-            if (hover) {
-              count = hover;
-            }
-            return (
-              <a key={i} href='#' className={userStars ? styles.userStars : ''}>
-                {i <= count ? (
-                  <FontAwesomeIcon
-                    onMouseOver={() => {
-                      setHover(i);
-                    }}
-                    onMouseLeave={() => {
-                      setHover(null);
-                    }}
-                    onClick={event => {
-                      event.preventDefault();
-                      return props.changeUserStars(id, i);
-                    }}
-                    icon={faStar}
-                  >
-                    {i} stars
-                  </FontAwesomeIcon>
-                ) : (
-                  <FontAwesomeIcon
-                    onMouseOver={() => {
-                      setHover(i);
-                    }}
-                    onMouseLeave={() => {
-                      setHover(null);
-                    }}
-                    onClick={event => {
-                      event.preventDefault();
-                      return props.changeUserStars(id, i);
-                    }}
-                    icon={farStar}
-                  >
-                    {i} stars
-                  </FontAwesomeIcon>
-                )}
-              </a>
-            );
-          })}
-        </div>
+        <Stars id={id} stars={stars} userStars={userStars} />
       </div>
       <div className={styles.line}></div>
       <div className={styles.actions}>
@@ -136,7 +85,6 @@ ProductBox.propTypes = {
   id: PropTypes.string,
   changeWishlist: PropTypes.func,
   changeCompareList: PropTypes.func,
-  changeUserStars: PropTypes.func,
   userStars: PropTypes.number,
 };
 
