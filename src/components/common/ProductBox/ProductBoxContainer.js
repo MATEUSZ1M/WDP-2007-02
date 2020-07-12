e788c95c611ae6ff76fe08d34c94267b6019da7e
Mikolaj-Krawiec <59422679+Mikolaj-Krawiec@users.noreply.github.com>
2020-07-12 23:46:35 +0200

    Clean up

--- src/components/common/ProductBox/ProductBoxContainer.js
+++ src/components/common/ProductBox/ProductBoxContainer.js
@@ -1,16 +1,11 @@
 import { connect } from 'react-redux';
 import ProductBox from './ProductBox';
 
-import {
-  changeWishlist,
-  changeCompareList,
-  changeUserStars,
-} from '../../../redux/productsRedux';
+import { changeWishlist, changeCompareList } from '../../../redux/productsRedux';
 
 const mapDispatchToProps = dispatch => ({
   changeWishlist: id => dispatch(changeWishlist(id)),
   changeCompareList: id => dispatch(changeCompareList(id)),
-  changeUserStars: (id, star) => dispatch(changeUserStars({ id: id, star: star })),
 });
 
 export default connect(null, mapDispatchToProps)(ProductBox);
