aa679438e40c632865c92412c092f61beb8359bf
Piotr Rajtar <piotrek.rajtar@gmail.com>
2020-07-15 00:08:41 +0200

    Implement PromotionBox feature

--- src/components/features/PromotionBox/PromotionBox.js
+++ src/components/features/PromotionBox/PromotionBox.js
@@ -3,16 +3,21 @@ import PropTypes from 'prop-types';
 
 import styles from './PromotionBox.module.scss';
 
+import PromotionItem from '../../common/PromotionItem/PromotionItem';
+
 class PromotionBox extends React.Component {
   render() {
     const { promotion } = this.props;
+
     return (
-      <div>
-        {promotion.map(image => (
-          <div key={image.id}>
-            <img src={image.img} alt={image.name} />
+      <div className={styles.root}>
+        <div className='container'>
+          <div className={'row ' + styles.promoContainer}>
+            {promotion.map(image => (
+              <PromotionItem key={image.id} {...image} />
+            ))}
           </div>
-        ))}
+        </div>
       </div>
     );
   }
@@ -21,7 +26,5 @@ class PromotionBox extends React.Component {
 PromotionBox.propTypes = {
   promotion: PropTypes.array,
 };
-
-PromotionBox.defaultProps = {};
 
 export default PromotionBox;
--- src/redux/initialState.js
+++ src/redux/initialState.js
@@ -305,21 +305,27 @@ const initialState = {
   promotion: [
     {
       id: 'sofa',
-      name: 'Orange sofa',
+      name: 'SOFA',
       img:
         'https://images.pexels.com/photos/1866149/pexels-photo-1866149.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
+      title: 'Guest room',
+      promo: '-20%',
     },
     {
       id: 'chair',
-      name: 'Gray chairs',
+      name: 'OFFICE CHAIR',
       img:
         'https://images.pexels.com/photos/923192/pexels-photo-923192.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
+      title: 'Collection',
+      promo: '$200.00',
     },
     {
       id: 'bed',
-      name: 'Bed',
+      name: 'FURNITURE',
       img:
         'https://images.pexels.com/photos/775219/pexels-photo-775219.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
+      title: 'Special collection',
+      promo: 'Save up 45% of',
     },
   ],
   cart: {
